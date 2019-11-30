f4d140537cc0b153f44aeacee074143ad468d95d
Victor Bassey <viktorbasi545@gmail.com>
2019-11-30 07:31:32 +0100

    Refactor customer controller to use get mapping annotation

--- src/com/tracker/controller/CustomerController.java
+++ src/com/tracker/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tracker.dao.CustomerDAO;
@@ -17,7 +18,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerDAO customerDAO;
 	
-	@RequestMapping("/list")
+	@GetMapping("/list")
 	public String listCustomers(Model model) {
 		
 		List<Customer> theCustomers = customerDAO.getCustomers();
