0fe9f38c9b27ae453f1bd536877e84d8ef2306b9
Victor Bassey <viktorbasi545@gmail.com>
2019-12-01 12:08:40 +0100

    Setup pointcut declarations and @before advice for creating logs

---
+++ src/com/tracker/aspect/CRMLoggingAspect.java
@@ -0,0 +1,43 @@
+package com.tracker.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+	
+	// setup logger
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	// setup pointcut declarations
+	@Pointcut("execution(* com.tracker.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* com.tracker.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.tracker.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {}
+	
+	// add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		// display the method being called
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("====>>> in @Before - calling method: " + theMethod);
+		
+		// display the arguments to the method
+	}
+	
+	// add @AferReturning advice
+	
+}
